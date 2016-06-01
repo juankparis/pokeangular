9c88cdc3594aa7a686f45841d97b5d62165d1017
juankparis <juan.k.paris@gmail.com>
2016-06-01 17:47:38 -0500

    implementacion de modulo para los controladores

--- app/js/app.js
+++ app/js/app.js
@@ -1,42 +1,44 @@
 (function(){
 	"use strict";
 	
-	var app = angular.module('pokeangular', []);
+	var app = angular.module('pokeangular', [
+		'pokeangular.controllers'
+		]);
 
-	app.controller('PokemonController', function(){
-		this.pokemon = {
-			id: "001",
-      		name: "Bulbasaur",
-      		species: "Seed Pokémon",
-      		type: [ "Grass", "Poison" ],
-      		height: "2′4″ (0.71m)",
-      		weight: "15.2 lbs (6.9 kg)",
-      		abilities: [ "Overgrow", "Chlorophyll"],
-		    stats: {
-		    	hp: 45,
-		    	attack: 49,
-		    	defense: 49,
-		    	"sp.atk": 65,
-		    	"sp.def": 65,
-		    	speed: 45,
-		    	total: 318
-		    },
-		    evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
-		};
-	});
+	// app.controller('PokemonController', function(){
+	// 	this.pokemon = {
+	// 		id: "001",
+ //      		name: "Bulbasaur",
+ //      		species: "Seed Pokémon",
+ //      		type: [ "Grass", "Poison" ],
+ //      		height: "2′4″ (0.71m)",
+ //      		weight: "15.2 lbs (6.9 kg)",
+ //      		abilities: [ "Overgrow", "Chlorophyll"],
+	// 	    stats: {
+	// 	    	hp: 45,
+	// 	    	attack: 49,
+	// 	    	defense: 49,
+	// 	    	"sp.atk": 65,
+	// 	    	"sp.def": 65,
+	// 	    	speed: 45,
+	// 	    	total: 318
+	// 	    },
+	// 	    evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
+	// 	};
+	// });
 
-	app.controller('TabController', function(){
-		this.tab = 1;
+	// app.controller('TabController', function(){
+	// 	this.tab = 1;
 
-		this.selectTab = function(tab){
-			this.tab = tab;
-		};
+	// 	this.selectTab = function(tab){
+	// 		this.tab = tab;
+	// 	};
 
-		this.isActive = function(tab){
-			return tab === this.tab;
-		};
+	// 	this.isActive = function(tab){
+	// 		return tab === this.tab;
+	// 	};
 
-	});
+	// });
 
 	app.filter('imageify', function () {
     	return function (input) {
