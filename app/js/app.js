93be691e1e122ebbcaafc9a482acc250d3dd20ab
juankparis <juan.k.paris@gmail.com>
2016-06-14 21:42:35 -0500

    ruta server arreglo

--- app/js/app.js
+++ app/js/app.js
@@ -13,15 +13,15 @@
 	app.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
 			.when('/',{
-				templateUrl: '/views/pokedex.html',
+				templateUrl: 'views/pokedex.html',
 				controller: 'pokedexController'
 			})
 			.when('/type/:type', {
-				templateUrl: '/views/pokedex.html',
+				templateUrl: 'views/pokedex.html',
 				controller: 'pokedexController'
 			})
 			.when('/pokemon/:name', {
-				templateUrl: '/views/pokemon.html',
+				templateUrl: 'views/pokemon.html',
 				controller: 'PokemonController'
 			})
 			.otherwise({ 
